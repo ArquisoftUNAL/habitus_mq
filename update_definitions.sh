b15d5e3f36320ab484a451d529edf89401b5b676
edgardanielgd <edgardanielgd123@gmail.com>
2023-09-30 00:02:24 -0500

    Fixed some permissions annoying errors

--- update_definitions.sh
+++ update_definitions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i 's~admin_password_hash~'$ADMIN_PASSWORD_HASH'~g' /etc/rabbitmq/definitions.json
 sed -i 's~w_notifications_password_hash~'$W_NOTIFICATIONS_PASSWORD_HASH'~g' /etc/rabbitmq/definitions.json
-sed -i 's~r_notifications_password_hash~'$R_NOTIFICATIONS_HASH'~g' /etc/rabbitmq/definitions.json
+sed -i 's~r_notifications_password_hash~'$R_NOTIFICATIONS_PASSWORD_HASH'~g' /etc/rabbitmq/definitions.json
 sed -i 's~w_new_habit_data_password_hash~'$W_NEW_HABIT_DATA_PASSWORD_HASH'~g' /etc/rabbitmq/definitions.json
 sed -i 's~r_new_habit_data_password_hash~'$R_NEW_HABIT_DATA_PASSWORD_HASH'~g' /etc/rabbitmq/definitions.json
 echo "Updated definitions.json"
